6de7a9fdfd21351e6a542968b362e20bb750c124
Documenter.jl <documenter@juliadocs.github.io>
2023-03-16 18:23:13 +0000

    build based on 7b574e5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CtrlVQE","category":"page"},{"location":"#CtrlVQE","page":"Home","title":"CtrlVQE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CtrlVQE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CtrlVQE]","category":"page"}]
+}
